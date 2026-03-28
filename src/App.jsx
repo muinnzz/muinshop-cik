5b300aacca000d94798a31fc93aa01e915c555cd
muinnzz <konohagakuredes@gmail.com>
2026-03-28 18:19:54 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#f5f7fb] flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Muinshop Cik Ready 🚀</h1>
+    </div>
+  );
+}
